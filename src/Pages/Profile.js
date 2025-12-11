f4558b281f9cae7736974a6393f996b4fe68df76
Ajai <ajailight7646337@gmail.com>
2025-12-11 11:41:20 +0530

    3rd commit

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -1,4 +1,11 @@
 function Profile(){
-    return <h1>Profile</h1>
+    return(
+        <div className="profile-container">
+            <h1 className="profile-h1">Profile</h1>
+            <div className="profile-info">
+                <h2 className="profile-h2">User Information</h2>
+            </div>
+        </div>
+    ); 
 }
 export default Profile
--- src/Pages/Tasks.js
+++ src/Pages/Tasks.js
@@ -1,9 +1,11 @@
 function Tasks() {
   return (
   <div className='tasks-container'>
-    <h1 className="tasks-h1">Tasks Page</h1>
-    <div className='tasks-addTasks'>
-      <button className='tasks-button'>Add Task</button>
+    <h1 className='tasks-h1'>Tasks Page</h1>
+    <div className="task-addButton"><button className='addButton'>Add Task</button></div>
+    
+    <div className='tasks-list'>
+      <div className='task-item'>Task 1: Complete assignment</div>
     </div>
   </div>
   );
