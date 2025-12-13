0b6a4816e1cd10eff765a4ccb4b5c95bdbd670b4
Ajai-L16 <ajai.l162006@gmail.com>
2025-12-13 08:59:36 +0530

    13th commit

--- src/Pages/Notes.js
+++ src/Pages/Notes.js
@@ -1,7 +1,35 @@
-function Notes() {
-  return (
-  <h1 className="">Notes Page</h1>
+import ".././Styles/Notes.css"
+import {useForm} from "react-hook-form";
 
+
+function Notes() {
+  const {
+      register,
+      handleSubmit,
+      formState: { errors },
+  } = useForm();
+
+   const onSubmitHandler = (e) => {
+        console.log(e);
+        alert("data added successfully");
+    };
+
+  return (
+    <div className="notes">
+      <h1 className="">Notes Page</h1>
+         <form onSubmit={handleSubmit(onSubmitHandler)}>
+                <label>Author: </label>
+                <input {...register("name")}type="text" />
+                <label>Date: </label>
+                <input {...register("date")} type="date" />
+                <label>Subject: </label>
+                <input {...register("subject")} type="text" />
+                <label>Description</label>
+                <input {...register("description")} type="text" />
+                <button type="submit">Add Notes</button>
+            </form>
+
+    </div>
   );
 }
 export default Notes;
